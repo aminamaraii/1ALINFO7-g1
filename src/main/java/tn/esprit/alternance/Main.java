ce6662bfae171b283eec66284a8318e52b30e969
amina maraii <amina.marai@avaxia-group.com>
2023-02-15 10:42:05 +0100

    deleting

--- src/main/java/tn/esprit/alternance/Main.java
+++ src/main/java/tn/esprit/alternance/Main.java
@@ -8,27 +8,6 @@ public class Main {
         Departement dept3 = new Departement(3, "departement 3");
         Departement dept4 = new Departement(4, "departement 4");
 
-//<<<<<<< HEAD
-       System.out.println(dept1);
-       System.out.println(dept2);
-       System.out.println(dept3);
-       System.out.println(dept4);
-//<<<<<<< HEAD
-       
-       System.out.println("*****************");
-       
-       Employe emp1 = new Employe(999,000,"houwayda","gouia");
-       Employe emp2 = new Employe(888,111,"nour","bachchouch");
-       Employe emp3 = new Employe(777,222,"yasmine","marzouk");
-       Employe emp4 = new Employe(666,333,"amina","marai");
-       Employe emp5 = new Employe(555,444,"abdelhak","amami");
-       
-       System.out.println(emp1);
-       System.out.println(emp2);
-       System.out.println(emp3);
-       System.out.println(emp4);
-       System.out.println(emp5);
-//=======
       
 //>>>>>>> main
 //=======
